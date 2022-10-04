610bce5d56c388c0c56cb79fd085ee067bd9db63
cloud1160 <claudia.adler@tu-dresden.de>
2022-10-04 15:45:07 +0200

    first mp4 was generated with this code

--- audio.py
+++ audio.py
@@ -1,32 +1,22 @@
 #!/usr/bin/python3.9
-from cgitb import text
+
 import re
 import os
 import sys
-# from ctts import cTTS
 import distutils.spawn
 def dier(msg):
     sys.exit(msg)
 
-# cTTS.synthesizeToFile("output.wav", "Das ist ein Test.")
-
-# os.system("tts")
-
-# check = os.system("echo $?")
-# if check:
-#     print("Is installed")
-# else:
-#     print("Did not work.")
-
-alarm = distutils.spawn.find_executable("tts")
-if alarm:
-    print(alarm)
+alert = distutils.spawn.find_executable("tts")
+if alert:
+    print(alert)
 else:
     os.system("pip install tts==0.8.0")
 
-# wie kann der server gestarted werden und gleichzeitig das skript weiterlaufen
-# os.system("tts-server --model_name tts_models/de/thorsten/vits")
-
+if distutils.spawn.find_executable("sox"):
+    print("sox was found")
+else:
+    os.system("sudo apt install sox")
 
 filename = "video.txt"
 file1 = open(filename, 'r')
@@ -42,22 +32,62 @@ count = 0
 for line in Lines:
     count += 1
     line.strip()
-    # print(line)
     if comment_regex.match(line):
         print("Ignoring comment line " + line)
     elif png_regex.match(line):
-        tokens.append({"image": line})
-        # video_data['image'] = line
+        tokens.append({"image": line[6:]})
     elif video_regex.match(line):
         tokens.append({"video": line[6:]})
-        # video_data['video'] = line
     else:
-        # print("Line{}: {}".format(count, line))
         pattern = r'\s*[.!?]+\s+'
         l = list(filter(None, re.split(pattern, line)))
         tokens.append({"text": l})
-        # video_data['text'] = "output%s.wav" % count
         # os.system("tts --model_name tts_models/de/thorsten/tacotron2-DDC --out_path output%s.wav --text '%s'" % (count, line))
 print(tokens)
-# os.system("ffmpeg -framerate 1 -pattern_type glob -i '*.png' -i output.wav \
-    # -c:v libx264 -pix_fmt yuv420p out5.mp4")
+
+count = 0
+for token in tokens:
+    if "text" in token.keys():
+        token["audio"] = []
+        for sentence in token["text"]:
+            os.system("tts --model_name tts_models/de/thorsten/tacotron2-DDC \
+                --out_path output%s.wav --text '%s'" % (count, sentence))
+            token["audio"].append("output%s.wav" % count)
+            count += 1
+
+print(tokens)
+
+token_is_text = False
+count = 0
+vid_count = 0
+file_entry = ""
+for token in tokens:
+    if token_is_text and "audio" not in token.keys():
+        os.system("sox %s conout%s.wav" % (filenames, count))
+        current_audio = "conout%s.wav" %count
+    if "audio" in token.keys():
+        if not token_is_text:
+            filenames = ""
+        token_is_text = True
+
+        for filename in token["audio"]:
+            filenames += filename + " "
+    if "image" in token.keys():
+        token_is_text = False
+        print(current_audio)
+        cmd = '''ffmpeg -i %s -i %s vidout%s.flv''' % (token["image"], current_audio, vid_count)
+        cmd = cmd.replace("\n", "")
+        file_entry += "file vidout%s.flv\n" % vid_count
+        os.system(cmd)
+        vid_count += 1
+
+    if "video" in token.keys():
+        token_is_text = False
+    count += 1
+
+with open("vids.txt", "w") as f:
+    f.write(file_entry)
+
+os.system("ffmpeg -f concat -i vids.txt -c copy final.flv")
+
+os.system("ffmpeg -i final.flv -qscale 0 -ar 22050 final.mp4")

2d7a28545285449baea8f606bee91b9bddf7ee96
cloud1160 <claudia.adler@tu-dresden.de>
2022-10-06 08:31:40 +0200

    concatening video files works

--- audio.py
+++ audio.py
@@ -4,26 +4,43 @@ import re
 import os
 import sys
 import distutils.spawn
+
+
 def dier(msg):
     sys.exit(msg)
 
-alert = distutils.spawn.find_executable("tts")
-if alert:
-    print(alert)
+
+def file_exists(entry):
+    if os.path.exists(entry.replace("\n", "")):
+        print("The file %s exists" % entry.replace("\n", ""))
+        return True
+    else:
+        dier("The file %s doesn't exist programm stops." %
+             entry.replace("\n", ""))
+
+
+replace_words = {"gpus": "ge pe us", "ssh": "s s h"}
+
+if distutils.spawn.find_executable("tts"):
+    print("tts is installed: continue")
 else:
     os.system("pip install tts==0.8.0")
 
 if distutils.spawn.find_executable("sox"):
-    print("sox was found")
+    print("sox is installed: continue")
 else:
     os.system("sudo apt install sox")
 
 filename = "video.txt"
-file1 = open(filename, 'r')
-Lines = file1.readlines()
+try:
+    with open(filename, 'r') as file1:
+        Lines = file1.readlines()
+except:
+    print("create and write a video.txt file")
+
 comment_regex = re.compile("^\s*#")
-png_regex = re.compile("^image=\w+.\w+")
-video_regex = re.compile("^video=\w+.mp4")
+png_regex = re.compile("^image=.+\.\w+")
+video_regex = re.compile("^video=.+")
 
 tokens = []
 video_data = {}
@@ -33,12 +50,30 @@ for line in Lines:
     count += 1
     line.strip()
     if comment_regex.match(line):
-        print("Ignoring comment line " + line)
+        pass
+        # print("Ignoring comment line " + line)
     elif png_regex.match(line):
+        print(line[6:].replace("\n", ""))
         tokens.append({"image": line[6:]})
+        file_exists(line[6:])
+        # if os.path.exists(line[6:].replace("\n", "")):
+        #     print("The file %s exists" % line[6:].replace("\n", ""))
+        # else:
+        #     dier("The file %s doesn't exist programm stops." %
+        #          line[6:].replace("\n", ""))
     elif video_regex.match(line):
         tokens.append({"video": line[6:]})
+        file_exists(line[6:])
+        # if os.path.exists(line[6:]):
+        #     print("The file %s exists" % line[6:])
+        # else:
+        #     dier("The file %s doesn't exist programm stops." %
+        #          line[6:].replace("\n", ""))
     else:
+        for word in replace_words:
+            if word in line:
+                line = line.replace(word, replace_words[word])
+                # dier(word + " " + replace_words[word])
         pattern = r'\s*[.!?]+\s+'
         l = list(filter(None, re.split(pattern, line)))
         tokens.append({"text": l})
@@ -50,8 +85,8 @@ for token in tokens:
     if "text" in token.keys():
         token["audio"] = []
         for sentence in token["text"]:
-            os.system("tts --model_name tts_models/de/thorsten/tacotron2-DDC \
-                --out_path output%s.wav --text '%s'" % (count, sentence))
+            # os.system("tts --model_name tts_models/de/thorsten/tacotron2-DDC \
+            #     --out_path output%s.wav --text '%s'" % (count, sentence))
             token["audio"].append("output%s.wav" % count)
             count += 1
 
@@ -64,7 +99,7 @@ file_entry = ""
 for token in tokens:
     if token_is_text and "audio" not in token.keys():
         os.system("sox %s conout%s.wav" % (filenames, count))
-        current_audio = "conout%s.wav" %count
+        current_audio = "conout%s.wav" % count
     if "audio" in token.keys():
         if not token_is_text:
             filenames = ""
@@ -75,14 +110,21 @@ for token in tokens:
     if "image" in token.keys():
         token_is_text = False
         print(current_audio)
-        cmd = '''ffmpeg -i %s -i %s vidout%s.flv''' % (token["image"], current_audio, vid_count)
+        cmd = '''ffmpeg -i %s -i %s vidout%s.flv''' % (
+            token["image"], current_audio, vid_count)
         cmd = cmd.replace("\n", "")
         file_entry += "file vidout%s.flv\n" % vid_count
         os.system(cmd)
         vid_count += 1
 
     if "video" in token.keys():
+        print("--------- Video ist drin! Welches? %s ----------------" %
+              token["video"])
         token_is_text = False
+        cmd = "ffmpeg -i %s %s.flv" % (token["video"], token["video"][:3])
+        cmd = cmd.replace("\n", "")
+        os.system(cmd)
+        file_entry += "file %s.flv\n" % token["video"][:3]
     count += 1
 
 with open("vids.txt", "w") as f:
@@ -90,4 +132,7 @@ with open("vids.txt", "w") as f:
 
 os.system("ffmpeg -f concat -i vids.txt -c copy final.flv")
 
-os.system("ffmpeg -i final.flv -qscale 0 -ar 22050 final.mp4")
+os.system("ffmpeg -i final.flv -c:a copy -c:v mpeg4 final.mp4")
+
+# works but the length is always ten seconds
+# ffmpeg -loop 1 -i hongkong.png -i conout1.wav -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -to 00:00:10 b.mp4

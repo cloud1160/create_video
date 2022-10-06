b68e4bf1fb9dd7763db7a975c1bd492596d2d1c2
cloud1160 <claudia.adler@tu-dresden.de>
2022-10-06 11:05:39 +0200

    implemented cmdl parameters, prevent malcode execu

--- audio.py
+++ audio.py
@@ -5,6 +5,7 @@ from os import path, makedirs, system
 import sys
 import distutils.spawn
 from hashlib import md5
+import argparse
 
 
 def dier(msg):
@@ -29,22 +30,39 @@ def is_installed(program_name, install_command):
 
 replace_words = {"gpus": "ge pe us", "ssh": "s s h"}
 
+ap = argparse.ArgumentParser()
+
+ap.add_argument("--file")
+ap.add_argument("--overwrite")
+args = vars(ap.parse_args())
+
+overwrite = ""
+if args["overwrite"] == "y":
+    overwrite = " -y"
+
 is_installed("tts", "pip install tts==0.8.0")
 is_installed("sox", "sudo apt install sox")
 
 if not path.exists("tmp"):
     makedirs("tmp")
 
+Lines = []
 filename = "video.txt"
+# dier(args["file"])
+if file_exists(args["file"]):
+    # dier("file %s exists" % args["file"])
+    filename = args["file"]
+else:
+    dier("%s does not exist." % filename)
 try:
     with open(filename, 'r') as file1:
         Lines = file1.readlines()
 except:
-    print("create and write a video.txt file")
+    dier("create and write a video.txt file")
 
 comment_regex = re.compile("^\s*#")
-png_regex = re.compile("^image=.+\.\w+")
-video_regex = re.compile("^video=.+")
+png_regex = re.compile("^image=(.+\.\w+)$")
+video_regex = re.compile("^video=(.+)$")
 
 tokens = []
 video_data = {}
@@ -55,13 +73,14 @@ for line in Lines:
     line.strip()
     if comment_regex.match(line):
         pass
-    elif png_regex.match(line):
-        print(line[6:].replace("\n", ""))
-        tokens.append({"image": line[6:]})
-        file_exists(line[6:])
-    elif video_regex.match(line):
-        tokens.append({"video": line[6:]})
-        file_exists(line[6:])
+    elif fname := png_regex.match(line):
+        # print(line[6:].replace("\n", ""))
+        print(fname.group(1))
+        tokens.append({"image": fname.group(1)})
+        file_exists(fname.group(1))
+    elif fname := video_regex.match(line):
+        tokens.append({"video": fname.group(1)})
+        file_exists(fname.group(1))
     else:
         for word in replace_words:
             if word in line:
@@ -78,6 +97,7 @@ for token in tokens:
 
         for sentence in token["text"]:
             md5_hash = md5(sentence.encode('utf-8')).hexdigest()
+            sentence = sentence.replace("'", "")
             if path.exists("tmp/%s.wav" % md5_hash):
                 print("Audio '%s' was already created" % sentence)
             else:
@@ -106,9 +126,9 @@ for token in tokens:
     if "image" in token.keys():
         token_is_text = False
         print(current_audio)
-        cmd = '''ffmpeg -i %s -i %s vidout%s.flv''' % (
-            token["image"], current_audio, vid_count)
-        cmd = cmd.replace("\n", "")
+        cmd = 'ffmpeg -i %s -i %s vidout%s.flv%s' % (
+            token["image"], current_audio, vid_count, overwrite)
+        cmd = cmd.replace("\n", "").replace("'", "")
         file_entry += "file vidout%s.flv\n" % vid_count
         system(cmd)
         vid_count += 1
@@ -117,8 +137,9 @@ for token in tokens:
         print("--------- Video ist drin! Welches? %s ----------------" %
               token["video"])
         token_is_text = False
-        cmd = "ffmpeg -i %s %s.flv" % (token["video"], token["video"][:3])
-        cmd = cmd.replace("\n", "")
+        cmd = "ffmpeg -i %s %s.flv%s" % (token["video"],
+                                         token["video"][:3], overwrite)
+        cmd = cmd.replace("\n", "").replace("'", " ")
         system(cmd)
         file_entry += "file %s.flv\n" % token["video"][:3]
     count += 1
@@ -126,6 +147,6 @@ for token in tokens:
 with open("vids.txt", "w") as f:
     f.write(file_entry)
 
-system("ffmpeg -f concat -i vids.txt -c copy final.flv")
+system("ffmpeg -f concat -i vids.txt -c copy final.flv%s" % overwrite)
 
-system("ffmpeg -i final.flv -c:a copy -c:v mpeg4 final.mp4")
+system("ffmpeg -i final.flv -c:a copy -c:v mpeg4 final.mp4%s" % overwrite)
